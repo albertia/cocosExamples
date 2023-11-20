eea96e4ebba8fc8820e5e35aa4f8a1488fb9dcdc
Alexandre Albertí Buendia <alex@iMac-de-Alexandre.local>
2023-11-20 18:15:57 +0100

    laser beam and out of bounds prefab with lasers

--- OmNom_In_Multiverse_Of_Candy/assets/scripts/DeathTouch.ts
+++ OmNom_In_Multiverse_Of_Candy/assets/scripts/DeathTouch.ts
@@ -1,10 +1,13 @@
-import { _decorator, CircleCollider2D, Component, Node } from 'cc';
+import { _decorator, BoxCollider2D, CircleCollider2D, Collider2D, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('DeathTouch')
 export class DeathTouch extends Component {
     start() {
-        let collider = this.node.getComponent(CircleCollider2D);
+        let collider:Collider2D = this.node.getComponent(CircleCollider2D);
+        if (!collider) {
+            collider = this.node.getComponent(BoxCollider2D);
+        }
         collider.name = 'deathTouch'
         collider.apply();
     }
