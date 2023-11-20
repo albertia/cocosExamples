714fbebe74dd8c2d7b7dd69ce20b0020fa7c4db3
Alexandre Albertí Buendia <alex@iMac-de-Alexandre.local>
2023-11-20 12:25:11 +0100

    bubble drag and drop

--- OmNom_In_Multiverse_Of_Candy/assets/scripts/OmNomBubble.ts
+++ OmNom_In_Multiverse_Of_Candy/assets/scripts/OmNomBubble.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, instantiate, Node, NodeEventType, Prefab } from 'cc';
+import { _decorator, Component, instantiate, Node, NodeEventType, Prefab, Vec2, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('OmNomBubble')
@@ -8,7 +8,11 @@ export class OmNomBubble extends Component {
     @property(Node)
     public gameNode:Node;
 
+    private bubbleMoved = false;
+
     start() {
+        this.node.on(NodeEventType.TOUCH_START, this.startDrag, this);
+        this.node.on(NodeEventType.TOUCH_MOVE, this.moveBubble, this);
         this.node.on(NodeEventType.TOUCH_END, this.startGame, this);
 
     }
@@ -17,11 +21,24 @@ export class OmNomBubble extends Component {
         
     }
 
-    public startGame() {
-        const omnom = instantiate(this.omnomPrefab);
-        omnom.position = this.node.position;
-        this.gameNode.addChild(omnom);
-        this.node.destroy();
+    startDrag() {
+        this.bubbleMoved = false;
+    }
+
+    moveBubble(event) {
+        this.bubbleMoved = true;
+        var moved:Vec2 = event.getUIDelta();
+        this.node.position = new Vec3(this.node.position.x + moved.x, this.node.position.y + moved.y)
+    }
+
+
+    startGame() {
+        if (!this.bubbleMoved) {
+            const omnom = instantiate(this.omnomPrefab);
+            omnom.position = this.node.position;
+            this.gameNode.addChild(omnom);
+            this.node.destroy();
+        }
     }
 
 }
