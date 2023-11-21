7e2e1b37e34b35ad6043b398a9b885c4bbcd37a1
Guillermo Monfort Pardo <guillermom@zeptolab.com>
2023-11-21 11:10:56 +0100

    Basic portals

--- OmNom_In_Multiverse_Of_Candy/assets/scripts/LevelMechanics/LevelMechanicManager.ts
+++ OmNom_In_Multiverse_Of_Candy/assets/scripts/LevelMechanics/LevelMechanicManager.ts
@@ -1,6 +1,7 @@
 import { Component, _decorator } from 'cc';
 import { LevelMechanicSettingsDisplay } from '../LevelMechanicSettings/LevelMechanicSettingsDisplay';
 import { MechanicSelectedEvent } from './MechanicSelectedEvent';
+import { PortalMechanic } from './PortalMechanic';
 const { ccclass, property } = _decorator;
 
 @ccclass('LevelMechanicManager')
@@ -8,6 +9,8 @@ export class LevelMechanicManager extends Component {
 
     @property({ type: LevelMechanicSettingsDisplay })
     private levelMechanicSettingsDisplay: LevelMechanicSettingsDisplay | null = null;
+
+    public static portalMechanics: PortalMechanic[] = [];
 
     start() {
         this.node.on('mechanic', this.onMechanicSelected, this);
@@ -23,6 +26,31 @@ export class LevelMechanicManager extends Component {
         event.propagationStopped = true;
 
         this.levelMechanicSettingsDisplay.show(event.levelMechanic);
+    }
+
+    static registerPortalMechanic(portalMechanic: PortalMechanic) {
+        this.portalMechanics.push(portalMechanic);
+    }
+
+    static unregisterPortalMechanic(portalMechanic: PortalMechanic) {
+        for (let i = 0; i < this.portalMechanics.length; i++) {
+            if (this.portalMechanics[i] == portalMechanic) {
+                this.portalMechanics.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+    static getConnectedPortals(portalMechanic: PortalMechanic) {
+        let connectedPortals: PortalMechanic[] = [];
+
+        for (let currentPortal of this.portalMechanics) {
+            if (currentPortal != portalMechanic && currentPortal.levelMechanicColor == portalMechanic.levelMechanicColor) {
+                connectedPortals.push(currentPortal)
+            }
+        }
+
+        return connectedPortals;
     }
 }
 
