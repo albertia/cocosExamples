7b888acdd99284e42026d15c7cffc9e2ebdc28b0
Guillermo Monfort Pardo <guillermom@zeptolab.com>
2023-11-23 14:51:20 +0100

    Candy animation

--- OmNom_In_Multiverse_Of_Candy/assets/scripts/Candy.ts
+++ OmNom_In_Multiverse_Of_Candy/assets/scripts/Candy.ts
@@ -1,17 +1,25 @@
-import { _decorator, CircleCollider2D, Component } from 'cc';
+import { CircleCollider2D, Component, Node, Vec3, _decorator, tween } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Candy')
 export class Candy extends Component {
 
+    @property(Node)
+    private visualsNode: Node;
+
     start() {
         let collider = this.node.getComponent(CircleCollider2D);
         collider.name = 'candy'
         collider.apply();
-    }
 
-    update(deltaTime: number) {
+        this.visualsNode.setPosition(new Vec3(0, -5, 0));
 
+        tween(this.visualsNode)
+            .by(1, { position: new Vec3(0, 10, 0) }, { easing: 'sineInOut' })
+            .by(1, { position: new Vec3(0, -10, 0) }, { easing: 'sineInOut' })
+            .union()
+            .repeatForever()
+            .start();
     }
 }
 
