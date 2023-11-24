c18deae0b93d447df8a840d8cab60dbab722c603
Guillermo Monfort Pardo <guillermom@zeptolab.com>
2023-11-24 09:25:46 +0100

    NonOverlappingItem improvements

--- OmNom_In_Multiverse_Of_Candy/assets/scripts/NonOverlappingItem.ts
+++ OmNom_In_Multiverse_Of_Candy/assets/scripts/NonOverlappingItem.ts
@@ -1,32 +1,46 @@
-import { _decorator, BoxCollider2D, CircleCollider2D, Collider2D, Color, Component, Contact2DType, director, ERigidBody2DType, IPhysics2DContact, Node, PolygonCollider2D, RigidBody, RigidBody2D, Sprite } from 'cc';
-import { GameManager } from './GameManager';
+import { _decorator, CircleCollider2D, Collider2D, Color, Component, Contact2DType, director, ERigidBody2DType, IPhysics2DContact, RigidBody2D, Sprite } from 'cc';
+import { GameManager, GameState } from './GameManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('NonOverlappingItem')
 export class NonOverlappingItem extends Component {
 
-    private gameStarted:boolean = false;
-    private collider:Collider2D;
-    private collisions:number = 0;
+    private gameStarted: boolean = false;
+    private collider: Collider2D;
+    private rigidbody: RigidBody2D;
+    private collisions: number = 0;
     private collisionsAllowed = ["gravityField", "blackHole", "omnom"]
 
-    private originalEnable:boolean;
-    private originalType:ERigidBody2DType;
-    private originalContactListener:boolean;
+    private originalEnable: boolean;
+    private originalType: ERigidBody2DType;
+    private originalContactListener: boolean;
 
     start() {
         director.on('bubbleExploded', this.onBubbleExploded, this);
-        GameManager.eventTarget.on('gameStateChanged', this.onBubbleRecreated, this);
+        GameManager.eventTarget.on('gameStateChanged', this.onGameStateChanged, this);
+
+        this.collider = this.node.getComponent(CircleCollider2D);
+        this.rigidbody = this.node.getComponent(RigidBody2D);
+        this.originalEnable = this.collider.enabled;
+        this.originalType = this.rigidbody.type;
+        this.originalContactListener = this.rigidbody.enabledContactListener;
+
+
         this.onBubbleRecreated();
     }
 
     update(deltaTime: number) {
-        
+
+    }
+
+    onGameStateChanged(gameState: GameState) {
+        if (gameState == GameState.Editing) {
+            this.onBubbleRecreated();
+        }
     }
 
     onBubbleRecreated() {
         this.gameStarted = false;
-        this.collider = this.node.getComponent(CircleCollider2D);
         /* We will ise only circle colliders for this. In the case of polygon one, for the bounce, we will enable/disable it.
         if (!this.collider) {
             this.collider = this.node.getComponent(BoxCollider2D);
@@ -35,18 +49,15 @@ export class NonOverlappingItem extends Component {
             this.collider = this.node.getComponent(PolygonCollider2D);
         }
         */
-        this.originalEnable = this.collider.enabled;
         this.collider.enabled = true;
         this.collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
         this.collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
         this.collider.apply();
-        
-        this.originalType = this.node.getComponent(RigidBody2D).type;
-        this.originalContactListener = this.node.getComponent(RigidBody2D).enabledContactListener;
 
-        this.node.getComponent(RigidBody2D).enabledContactListener = true;
-        this.node.getComponent(RigidBody2D).allowSleep = false;
-        this.node.getComponent(RigidBody2D).type = ERigidBody2DType.Dynamic;
+
+        this.rigidbody.enabledContactListener = true;
+        this.rigidbody.allowSleep = false;
+        this.rigidbody.type = ERigidBody2DType.Dynamic;
     }
 
     onBubbleExploded() {
@@ -54,8 +65,8 @@ export class NonOverlappingItem extends Component {
         if (this.collisions > 0) {
             this.node.destroy();
         }
-        this.node.getComponent(RigidBody2D).enabledContactListener = this.originalContactListener;
-        this.node.getComponent(RigidBody2D).type = this.originalType;
+        this.rigidbody.enabledContactListener = this.originalContactListener;
+        this.rigidbody.type = this.originalType;
         this.collider.enabled = this.originalEnable;
         this.collider.apply();
     }
